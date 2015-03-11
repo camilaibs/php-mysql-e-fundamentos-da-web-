16c4de8dc8ce862287fbbf32258f335115fff64d
camilaibs <camilaibs@gmail.com>
2015-03-11 15:34:29 -0300

    seleção no banco

--- loja/cabecalho.php
+++ loja/cabecalho.php
@@ -18,6 +18,9 @@
                         <a href="produto-formulario.php">Adiciona Produto</a>
                     </li>
                     <li>
+                        <a href="produto-lista.php">Produtos</a>
+                    </li>
+                    <li>
                         <a href="sobre.php">Sobre</a>
                     </li>
                 </ul>
