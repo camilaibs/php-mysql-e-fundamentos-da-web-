0fef2afb17f3a316589ca8871286610d24bf831a
camilaibs <camilaibs@gmail.com>
2015-03-11 14:46:49 -0300

    inserção no banco

--- loja/adiciona-produto.php
+++ loja/adiciona-produto.php
@@ -2,6 +2,18 @@
     <?php
         $nome = $_GET["nome"];
         $preco = $_GET["preco"];
+
+        $query = "insert into produtos(nome, preco) values ('{$nome}', '{$preco}')";
+        $conexao = mysqli_connect('localhost', 'root', '', 'loja');
+        if(mysqli_query($conexao, $query)){
+    ?>
+    <p class="alert-success">Produto <?= $nome; ?>, <?= $preco; ?> adicionado com sucesso!</p>
+    <?php
+        }else{
+    ?>
+    <p class="alert-danger">O produto <?= $nome; ?> não foi adicionado</p>
+    <?php
+        }
     ?>
     <p class="alert-success">
         Produto <?php echo $nome; ?>, <?php echo $preco; ?> adicionado com sucesso!
